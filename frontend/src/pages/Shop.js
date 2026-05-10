d6d9e03e3e8c828fdd81f7cd2d1257988afc3b54
katinnovationtech <168232612+katinnovationtech@users.noreply.github.com>
2026-05-09 20:05:42 -0400

    Update Shop.js

--- frontend/src/pages/Shop.js
+++ frontend/src/pages/Shop.js
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { CartContext } from '../context/CartContext';
-import './Shop.css';
+import './shop.css';
 import API_URL from '../config';
 
 const COLOR_MAP = {
